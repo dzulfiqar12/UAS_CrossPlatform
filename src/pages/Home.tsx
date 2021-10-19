badab3d80b1d8ffa47b83bb2e24a20c42243a38f
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-10-19 12:54:49 +0700

    Add an example of data rendering and filtering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,79 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonAvatar,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonPage,
+  IonSegment,
+  IonSegmentButton,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import { useEffect, useState } from 'react';
+
+import { fetchFirestore } from '../firebase';
+import { getMenu } from '../firebase/firestore';
+import type Menu from '../types/Menu';
 
 const Home = () => {
+  const [category, setCategory] = useState('Ala Carte' as 'Ala Carte' | 'Paket' | 'Go Home');
+  const [menu, setMenu] = useState([] as Menu[]);
+
+  useEffect(() => {
+    getMenu(fetchFirestore())
+      .then((res) => setMenu(res))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <IonSegment
+          value={category}
+          onIonChange={({ detail: { value } }: CustomEvent) => setCategory(value)}
+        >
+          <IonSegmentButton value="Ala Carte">
+            <IonLabel>Ala Carte</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="Paket">
+            <IonLabel>Paket</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="Go Home">
+            <IonLabel>Go Home</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+
+        <IonList>
+          <IonListHeader>
+            <IonTitle>Menu</IonTitle>
+          </IonListHeader>
+
+          {menu
+            .filter((item) => item.category === category)
+            .map((item) => (
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src={item.photo} alt={`Avatar of ${item.name}`} />
+                </IonAvatar>
+
+                <IonLabel>
+                  <h2>{item.name}</h2>
+                  <h3>{item.category}</h3>
+                  <p>{item.description}</p>
+                </IonLabel>
+              </IonItem>
+            ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
