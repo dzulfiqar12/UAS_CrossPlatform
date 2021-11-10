1991684801d2869bfc5eb120cc101ce1a54d1165
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-10 15:57:24 +0700

    ESLint and Prettier

--- src/components/ModalSingleMenu.tsx
+++ src/components/ModalSingleMenu.tsx
@@ -1,37 +1,51 @@
-import {IonModal, IonImg, IonHeader, IonContent, IonToolbar, IonTitle, IonBackButton, IonButton, IonItem, IonListHeader, IonButtons} from '@ionic/react';
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import './Modal.css';
+
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonItem,
+  IonModal,
+  IonToolbar,
+} from '@ionic/react';
+import { Dispatch, SetStateAction } from 'react';
 
 import type Menu from '../types/Menu';
-import "./Modal.css";
 
 type Props = {
   menu: Menu;
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
-}
+};
 
 const ModalSingleMenu = ({ menu, isOpen, setIsOpen }: Props) => {
   return (
     <IonModal isOpen={isOpen}>
       <IonHeader>
         <IonToolbar>
-        <h1>Detail Menu</h1>
-      <IonButtons slot="end">
-      <IonButton onClick={() => setIsOpen(false)}>Back</IonButton>
-      </IonButtons>
-      </IonToolbar>
+          <h1>Detail Menu</h1>
+          <IonButtons slot="end">
+            <IonButton onClick={() => setIsOpen(false)}>Back</IonButton>
+          </IonButtons>
+        </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonItem>
-         <p><IonImg className="gambar-1" src={menu.photo} /></p>
+          <p>
+            <IonImg className="gambar-1" src={menu.photo} />
+          </p>
           <h2 className="judul-menu">{menu.name}</h2>
           <h3 className="ket-menu1">{menu.description}</h3>
           <p className="ket-menu">{menu.price}</p>
-          <IonButton shape="round" className="button-cart">Add to Cart</IonButton>
+          <IonButton shape="round" className="button-cart">
+            Add to Cart
+          </IonButton>
         </IonItem>
       </IonContent>
     </IonModal>
   );
-}
+};
 
 export default ModalSingleMenu;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,18 +13,13 @@ import {
   IonSegmentButton,
   IonTitle,
   IonToolbar,
-  IonModal,
-  useIonModal,
 } from '@ionic/react';
 import { useEffect, useState } from 'react';
 
+import ModalSingleMenu from '../components/ModalSingleMenu';
 import { fetchFirestore } from '../firebase';
 import { getMenu } from '../firebase/firestore';
 import type Menu from '../types/Menu';
-
-import ModalExample from '../components/ModalSingleMenu';
-import React, {} from 'react';
-import ModalSingleMenu from '../components/ModalSingleMenu';
 
 export const Home: React.FC = () => {
   const [category, setCategory] = useState('Ala Carte' as 'Ala Carte' | 'Paket' | 'Go Home');
@@ -40,61 +35,62 @@ export const Home: React.FC = () => {
 
   return (
     <>
-    <ModalSingleMenu menu={chosenMenu} isOpen={showModal} setIsOpen={setShowModal} />
+      <ModalSingleMenu menu={chosenMenu} isOpen={showModal} setIsOpen={setShowModal} />
 
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-        
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
 
-      <IonContent fullscreen>
-        <IonSegment
-          value={category}
-          onIonChange={({ detail: { value } }: CustomEvent) => setCategory(value)}
-        >
-          <IonSegmentButton value="Ala Carte">
-            <IonLabel>Ala Carte</IonLabel>
-          </IonSegmentButton>
-          <IonSegmentButton value="Paket">
-            <IonLabel>Paket</IonLabel>
-          </IonSegmentButton>
-          <IonSegmentButton value="Go Home">
-            <IonLabel>Go Home</IonLabel>
-          </IonSegmentButton>
-        </IonSegment>
+        <IonContent fullscreen>
+          <IonSegment
+            value={category}
+            onIonChange={({ detail: { value } }: CustomEvent) => setCategory(value)}
+          >
+            <IonSegmentButton value="Ala Carte">
+              <IonLabel>Ala Carte</IonLabel>
+            </IonSegmentButton>
+            <IonSegmentButton value="Paket">
+              <IonLabel>Paket</IonLabel>
+            </IonSegmentButton>
+            <IonSegmentButton value="Go Home">
+              <IonLabel>Go Home</IonLabel>
+            </IonSegmentButton>
+          </IonSegment>
 
-        <IonList>
-          <IonListHeader>
-            <IonTitle>Menu</IonTitle>
-          </IonListHeader>
+          <IonList>
+            <IonListHeader>
+              <IonTitle>Menu</IonTitle>
+            </IonListHeader>
 
-          {menu
-            .filter((item) => item.category === category)
-            .map((item) => (
-            
-              <IonItem onClick={() => {
-                setChosenMenu(item);
-                setShowModal(true);
-              }} key={item.id}>
-                <IonAvatar slot="start">
-                  <IonImg src={item.photo} alt={`Avatar of ${item.name}`} />
-                </IonAvatar>
-                <IonLabel>
-                  <h2>{item.name}</h2>
-                  <h3>{item.category}</h3>
-                  <p>{item.description}</p>
-                  <IonButton>Add to Cart</IonButton>
-                </IonLabel>
-              </IonItem>
-            ))}
-        </IonList>
+            {menu
+              .filter((item) => item.category === category)
+              .map((item) => (
+                <IonItem
+                  onClick={() => {
+                    setChosenMenu(item);
+                    setShowModal(true);
+                  }}
+                  key={item.id}
+                >
+                  <IonAvatar slot="start">
+                    <IonImg src={item.photo} alt={`Avatar of ${item.name}`} />
+                  </IonAvatar>
+                  <IonLabel>
+                    <h2>{item.name}</h2>
+                    <h3>{item.category}</h3>
+                    <p>{item.description}</p>
+                    <IonButton>Add to Cart</IonButton>
+                  </IonLabel>
+                </IonItem>
+              ))}
+          </IonList>
 
-        <IonButton routerLink="/cart">Cart</IonButton>
-      </IonContent>
-    </IonPage>
+          <IonButton routerLink="/cart">Cart</IonButton>
+        </IonContent>
+      </IonPage>
     </>
   );
 };
