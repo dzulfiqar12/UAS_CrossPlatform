b4395c4cc6c013ecad94c48cf1a41d592bbf8270
Christian Widjaja <christianwidjaja04@gmail.com>
2021-11-10 17:48:16 +0700

    Refactor to state, change 'Logins' to 'Login'

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,19 @@
 import '../styles/Login.css';
 
 import { IonButton, IonContent, IonInput, IonPage } from '@ionic/react';
-import { useRef } from 'react';
+import { useState } from 'react';
 import { useHistory } from 'react-router';
 
 import { login } from '../firebase/auth';
 import routes from '../utils/routes';
 
-const Logins = () => {
-  const emailRef = useRef<HTMLIonInputElement>(null);
-  const passwordRef = useRef<HTMLIonInputElement>(null);
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const history = useHistory();
 
   const signIn = async () => {
-    const enteredEmail = emailRef.current!.value as string;
-    const enteredPassword = passwordRef.current!.value as string;
-
-    login(enteredEmail, enteredPassword)
+    login(email, password)
       .then(() => console.log('Login success!'))
       .then(() => history.replace(routes.home))
       .catch((err) => console.error(err));
@@ -31,17 +28,19 @@ const Logins = () => {
             name="email"
             type="text"
             placeholder="username"
-            ref={emailRef}
             color="light"
-          ></IonInput>
+            value={email}
+            onIonChange={({ detail: { value } }) => setEmail(value!)}
+          />
 
           <IonInput
             name="password"
             type="password"
             placeholder="password"
-            ref={passwordRef}
             color="light"
-          ></IonInput>
+            value={password}
+            onIonChange={({ detail: { value } }) => setPassword(value!)}
+          />
 
           <IonButton shape="round" fill="solid" className="login_button" onClick={signIn}>
             Login
@@ -52,4 +51,4 @@ const Logins = () => {
   );
 };
 
-export default Logins;
+export default Login;
