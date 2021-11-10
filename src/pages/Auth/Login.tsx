e4b58d4e4a5786ed58d153a71539908d30f75ed1
Christian Widjaja <christianwidjaja04@gmail.com>
2021-11-10 15:42:02 +0700

    Slight refactor in 'Login' to use functions

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,50 +1,50 @@
 import './css/login.css';
 
-import {
-  IonButton,
-  IonContent,
-  IonHeader,
-  IonInput,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from '@ionic/react';
-import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { useRef, useState } from 'react';
+import { IonButton, IonContent, IonInput, IonPage } from '@ionic/react';
+import { useRef } from 'react';
+import { useHistory } from 'react-router';
 
-import { auth } from '../../firebase/auth';
+import { fetchAuth } from '../../firebase';
+import { login } from '../../firebase/auth';
+import routes from '../../utils/routes';
 
 const Logins = () => {
   const emailRef = useRef<HTMLIonInputElement>(null);
   const passwordRef = useRef<HTMLIonInputElement>(null);
-  const login = async () => {
+  const history = useHistory();
+
+  const signIn = async () => {
     const enteredEmail = emailRef.current!.value as string;
-    const enteredPasswrod = passwordRef.current!.value as string;
-    const completeEmail = `${enteredEmail}@ayam-bebek-pak-boss.com`;
-    try {
-      const user = await signInWithEmailAndPassword(auth, completeEmail, enteredPasswrod);
-      console.log(user);
-    } catch (error) {
-      let errorMessage = 'Failed to do something exceptional';
-      if (error instanceof Error) {
-        errorMessage = error.message;
-      }
-      console.log(errorMessage);
-    }
+    const enteredPassword = passwordRef.current!.value as string;
+
+    login(fetchAuth(), enteredEmail, enteredPassword)
+      .then(() => console.log('Login success!'))
+      .then(() => history.replace(routes.home))
+      .catch((err) => console.error(err));
   };
+
   return (
     <IonPage>
       <IonContent fullscreen className="content">
         <div className="elipse"></div>
         <div className="login_form">
-          <IonInput name="email" type="text" placeholder="username" ref={emailRef}></IonInput>
+          <IonInput
+            name="email"
+            type="text"
+            placeholder="username"
+            ref={emailRef}
+            color="light"
+          ></IonInput>
+
           <IonInput
             name="password"
             type="password"
             placeholder="password"
             ref={passwordRef}
+            color="light"
           ></IonInput>
-          <IonButton shape="round" fill="solid" className="login_button" onClick={login}>
+
+          <IonButton shape="round" fill="solid" className="login_button" onClick={signIn}>
             Login
           </IonButton>
         </div>
