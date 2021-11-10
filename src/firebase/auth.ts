cb95be0ea098b3bbe6fad9fb3d408968d8b7324b
Christian Widjaja <christianwidjaja04@gmail.com>
2021-11-10 15:41:25 +0700

    Set up Firebase Authentication functions

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,18 +1,44 @@
-import { initializeApp } from '@firebase/app';
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
-import { Redirect } from 'react-router';
+import { Auth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
-import firebaseConfig from './config';
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
-    console.log(uid);
-    // ...
-  } else {
-    // <Redirect to="/login" />;
+/**
+ * Global error constant, give out generic error.
+ */
+const AuthError = new Error('Firebase Auth has encountered an error. Please try again later.');
+
+/**
+ * Logs in a user.
+ *
+ * @param auth - Firebase authentication service
+ * @param username - Username that is inputted
+ * @param password - A user's password
+ * @param dispatch - Setter to modify the context
+ */
+export const login = async (auth: Auth, username: string, password: string) => {
+  const completeEmail = `${username}@ayam-bebek-pak-boss.com`;
+
+  try {
+    await signInWithEmailAndPassword(auth, completeEmail, password);
+  } catch {
+    throw AuthError;
   }
-});
+};
+
+/**
+ * Logs out a user.
+ *
+ * @param auth - Firebase authentication service
+ */
+export const logout = async (auth: Auth) => {
+  try {
+    await signOut(auth);
+  } catch {
+    throw AuthError;
+  }
+};
+
+/**
+ * Gets the current user.
+ *
+ * @param auth - Firebase authentication service
+ */
+export const getUser = async (auth: Auth) => auth.currentUser;
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,7 @@
 import { getFirestore } from '@firebase/firestore';
 import { getStorage } from '@firebase/storage';
 import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 import firebaseConfig from './config';
 
@@ -24,3 +25,22 @@ export const fetchFirestore = () => getFirestore(initializeFirebaseApp());
  * @returns Cloud Storage instance
  */
 export const fetchStorage = () => getStorage(initializeFirebaseApp());
+
+/**
+ * Creates a single Authentication instance based on the default settings.
+ *
+ * @returns Authentication instance
+ */
+export const fetchAuth = () => getAuth(initializeFirebaseApp());
+
+/**
+ * Single listener to listen to the authentication observable.
+ */
+onAuthStateChanged(fetchAuth(), (user) => {
+  if (user) {
+    console.log('User is logged in with the following details:');
+    console.log(user);
+  } else {
+    console.log('User has been signed out.');
+  }
+});
