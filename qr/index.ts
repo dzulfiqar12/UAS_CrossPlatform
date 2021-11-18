dbdaa3e1bac77c5b03a220c85de6070b17ff39ce
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-18 10:31:46 +0700

    Add QR code as dependency and its generator

---
+++ qr/index.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import qr from 'qrcode';
+
+async function main() {
+  const tableName = '1A';
+  const websiteURL = 'https://ayam-bebek-pak-boss-dev--dev-m305ihi0.web.app/app/home';
+
+  try {
+    const base64String = await qr.toDataURL(`${websiteURL}/${tableName}`);
+    const base64Image = base64String.split(';base64').pop();
+
+    fs.writeFileSync(`${__dirname}/qrcode.png`, base64Image, { encoding: 'base64' });
+  } catch (err) {
+    throw err;
+  }
+}
+
+if (require.main === module) {
+  main()
+    .then(() => {
+      console.log("QR code generated successfully and stored in 'qrcode.png'!");
+      process.exit(0);
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+}
