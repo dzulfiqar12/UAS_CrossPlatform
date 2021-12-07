b6453f138defba886a1cb9ef13e8e11855992558
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-12-07 22:18:14 +0700

    Add dependencies for camera and PWA elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -11,6 +12,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+defineCustomElements(window);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
