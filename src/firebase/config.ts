3c7440b1648b6f329c533ed3dee9ef28d90f4078
dzulfiqar12 <dzulfiqar@student.umn.ac.id>
2021-12-08 00:02:55 +0700

    Replace firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,12 @@ import { FirebaseOptions } from '@firebase/app';
  * Firebase configuration defaults.
  */
 const firebaseConfig: FirebaseOptions = {
-  apiKey: 'AIzaSyA9eG5TPih1c8L2MNyBDcbXnv3ba8kwmWE',
-  authDomain: 'ayam-bebek-pak-boss-dev.firebaseapp.com',
-  projectId: 'ayam-bebek-pak-boss-dev',
-  storageBucket: 'ayam-bebek-pak-boss-dev.appspot.com',
-  messagingSenderId: '568795540694',
-  appId: '1:568795540694:web:0c874957391aa4b768ad77',
+  apiKey: 'AIzaSyBAysGy4kOHcVAHWL3ieAxq8RqpnhAy_-k',
+  authDomain: 'ayam-bebek-73e5e.firebaseapp.com',
+  projectId: 'ayam-bebek-73e5e',
+  storageBucket: 'ayam-bebek-73e5e.appspot.com',
+  messagingSenderId: '740696851012',
+  appId: '1:740696851012:web:4096a4ce2e3acc8d50f211',
 };
 
 export default firebaseConfig;
